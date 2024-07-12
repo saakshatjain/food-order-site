1653872d567a4a354e33ae0f28fc0c9f28dc512d
saakshatjain <123252511+saakshatjain@users.noreply.github.com>
2024-07-12 23:34:45 +0530

    User Signin added

--- frontend/food-site-frontend/src/App.tsx
+++ frontend/food-site-frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import './App.css'
-import UserSignup from "./components/UserSignup"
+import SignUp from './components/mui/user/Usersignup';
+import SignIn from './components/mui/user/Usersignin';
 function App() {
   return ( 
      <div>
          <BrowserRouter>
          <Routes>
-          <Route path="/" element={<UserSignup></UserSignup>}></Route>
+          <Route path="/signup" element={<SignUp></SignUp>}></Route>
+          <Route path="/signin" element={<SignIn></SignIn>}></Route>
          </Routes>
          </BrowserRouter>
      </div>
--- frontend/food-site-frontend/src/components/UserSignup.tsx
+++
@@ -1,8 +0,0 @@
-import SignUp from "./mui/user/Usersignup"
-export default function UserSignup(){
-    return(
-        <div>
-            <SignUp></SignUp>
-       </div>
-    )
-}
